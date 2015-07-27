5f58f27355654a17f805b33a3bb1c61324843371
Stephen "Zero" Chappell <Blastus@users.noreply.github.com>
2015-07-27 10:22:54 -0500

    Added some documentation to the program and added some terms to the dictionary.

--- qtpipe.py
+++ qtpipe.py
@@ -1,5 +1,13 @@
-__version__ = 1, 0, 0
-__date__ = '24 July 2015'
+#! /usr/bin/env python3
+
+"""Utility used for piping data over a network connection.
+
+This program is designed to create a connection with another running instance
+and pipe data from standard in to standard out. Run this program with either
+-h or --help as a command line argument for further instructional material."""
+
+__version__ = 1, 0, 1
+__date__ = '27 July 2015'
 __author__ = 'Stephen Paul Chappell'
 __credits__ = '''Jacob Bridges, for inspiring this program via cutiepipe.
 Please see https://github.com/jacobbridges/cutiepipe for more information.'''
@@ -15,6 +23,7 @@ STANDARD_IN, STANDARD_OUT, STANDARD_ERROR = range(3)
 
 
 def main():
+    """Connect with another process and simulate a pipe over a network."""
     link = make_link()
     try:
         if os.isatty(STANDARD_IN):
@@ -27,6 +36,7 @@ def main():
 
 
 def make_link():
+    """Create a client-server relationship to enable transfer of data."""
     address = parse_arguments()
     try:
         return socket.create_connection(address, 1)
@@ -50,6 +60,7 @@ def make_link():
 
 
 def parse_arguments():
+    """Check the command line for arguments effecting program parameters."""
     parser = argparse.ArgumentParser(description='Pipe data over a network.')
     parser.add_argument('host', nargs='?', default=DEFAULT_HOST, type=str,
                         help='name of computer to link with')
@@ -61,7 +72,14 @@ def parse_arguments():
     return arguments.host, arguments.port
 
 
+def raise_error(message):
+    """Convey error state in program to user and terminate execution."""
+    os.write(STANDARD_ERROR, '\n{}\n'.format(message).encode('ascii'))
+    raise SystemExit(1)
+
+
 def create_server(port):
+    """Construct a server waiting for a single incoming connection."""
     for family, kind, proto, _, address in socket.getaddrinfo(
             None, port, socket.AF_UNSPEC, socket.SOCK_STREAM, 0,
             socket.AI_PASSIVE):
@@ -80,12 +98,8 @@ def create_server(port):
     raise_error('could not create server socket')
 
 
-def raise_error(message):
-    os.write(STANDARD_ERROR, '\n{}\n'.format(message).encode('ascii'))
-    raise SystemExit(1)
-
-
 def pipe_from_link_to_standard_out(link):
+    """Collect all data from the socket and display it using standard out."""
     while True:
         buffer = link.recv(BUFFER_SIZE)
         if not buffer:
@@ -94,6 +108,7 @@ def pipe_from_link_to_standard_out(link):
 
 
 def pipe_from_standard_in_to_link(link):
+    """Communicate all data from standard in through the connected socket."""
     while True:
         buffer = os.read(STANDARD_IN, BUFFER_SIZE)
         if not buffer:
