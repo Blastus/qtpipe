af4ed7059393a52c3103ed810df27d7d5ba1eb85
Stephen "Zero" Chappell <Blastus@users.noreply.github.com>
2015-07-28 16:30:25 -0500

    Added an optional timeout command line argument, extra handling for errors, and more configuration settings.

--- qtpipe.py
+++ qtpipe.py
@@ -6,8 +6,8 @@ This program is designed to create a connection with another running instance
 and pipe data from standard in to standard out. Run this program with either
 -h or --help as a command line argument for further instructional material."""
 
-__version__ = 1, 0, 1
-__date__ = '27 July 2015'
+__version__ = 1, 0, 2
+__date__ = '28 July 2015'
 __author__ = 'Stephen Paul Chappell'
 __credits__ = '''Jacob Bridges, for inspiring this program via cutiepipe.
 Please see https://github.com/jacobbridges/cutiepipe for more information.'''
@@ -16,9 +16,13 @@ import argparse
 import os
 import socket
 
-BUFFER_SIZE = 1 << 12
-DEFAULT_HOST = 'localhost'
-DEFAULT_PORT = 10000
+DEFAULT_HOST = 'localhost'  # computer to connect to if not specified
+DEFAULT_PORT = 10000        # port to connect on if not specified
+DATA_TIMEOUT = 60           # maximum time to wait when sending or getting data
+CLIENT_TIMEOUT = 5          # maximum time allowed to connect to server
+SERVER_TIMEOUT = 60         # maximum time allowed for client to connect
+BUFFER_SIZE = 1 << 12       # most bytes sent or received at a time
+
 STANDARD_IN, STANDARD_OUT, STANDARD_ERROR = range(3)
 
 
@@ -30,6 +34,8 @@ def main():
             pipe_from_link_to_standard_out(link)
         else:
             pipe_from_standard_in_to_link(link)
+    except socket.timeout:
+        raise_error('could not process any data within the timeout period')
     finally:
         link.shutdown(socket.SHUT_RDWR)
         link.close()
@@ -37,10 +43,10 @@ def main():
 
 def make_link():
     """Create a client-server relationship to enable transfer of data."""
-    address = parse_arguments()
+    *address, timeout = parse_arguments()
     try:
-        return socket.create_connection(address, 1)
-    except (socket.gaierror, OverflowError) as error:
+        link = socket.create_connection(address, CLIENT_TIMEOUT)
+    except socket.gaierror as error:
         raise_error(str(error))
     except (socket.timeout, ConnectionRefusedError):
         host, port = address
@@ -48,28 +54,52 @@ def make_link():
             raise_error('could not connect to "{}" computer'.format(host))
         else:
             server = create_server(port)
-            server.settimeout(60)
+            server.settimeout(SERVER_TIMEOUT)
             try:
                 link, address = server.accept()
             except socket.timeout:
                 raise_error('could not connect and did not receive any data')
             else:
+                link.settimeout(timeout)
                 return link
             finally:
                 server.close()
+    else:
+        link.settimeout(timeout)
+        return link
 
 
 def parse_arguments():
     """Check the command line for arguments effecting program parameters."""
     parser = argparse.ArgumentParser(description='Pipe data over a network.')
-    parser.add_argument('host', nargs='?', default=DEFAULT_HOST, type=str,
-                        help='name of computer to link with')
-    parser.add_argument('port', nargs='?', default=DEFAULT_PORT, type=int,
-                        help='port used for the connection')
+    parser.add_argument('host', nargs='?', default=DEFAULT_HOST,
+                        type=str, help='name of computer to link with')
+    parser.add_argument('port', nargs='?', default=DEFAULT_PORT,
+                        type=port_integer, help='port used for the connection')
+    parser.add_argument('timeout', nargs='?', default=DATA_TIMEOUT,
+                        type=time_integer, help='time allowed for no activity')
     parser.add_argument('-v', '--version', action='version',
                         version='%(prog)s {}.{}.{}'.format(*__version__))
     arguments = parser.parse_args()
-    return arguments.host, arguments.port
+    return arguments.host, arguments.port, arguments.timeout
+
+
+def port_integer(data):
+    """Convert the data into a port number falling within a valid range."""
+    value = int(data)
+    if value not in range(1 << 10, 1 << 15):
+        raise ValueError('port must be in range of 1024 and 65535')
+    return value
+
+
+def time_integer(data):
+    """Change the data into a form suitable for setting a socket's timeout."""
+    if data.capitalize() == 'None':
+        return None
+    value = int(data)
+    if value < 0:
+        raise ValueError('timeout must be a non-negative integer')
+    return value
 
 
 def raise_error(message):
